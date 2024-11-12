109851b3fcabbcd7713d3e3839d50e11e38e68a5
Alison Wu <alison.wu23@gmail.com>
2024-11-12 17:16:33 +1000

    feat: 03-setter-injection

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,12 +3,6 @@ package com.luv2code.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-// Explicitly list base packages to scan
-//@SpringBootApplication(
-//		scanBasePackages = {"com/luv2code/springboot/demo/mycoolapp",
-//							"com/luv2code/springboot/demo/mycoolapp/util"})
-
 @SpringBootApplication
 public class MycoolappApplication {
 
