e0e17ccaa01851c2f1691d272e7cea39c0c76296
Alison Wu <alison.wu23@gmail.com>
2024-11-12 23:24:34 +1000

    feat: 08-bean-lifecycle-methods

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/common/TrackCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/common/TrackCoach.java
@@ -1,17 +1,27 @@
 package com.luv2code.springboot.demo.mycoolapp.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)    // Bean - the prototype scope
 public class TrackCoach implements Coach {
 
     public TrackCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
 
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k!";
