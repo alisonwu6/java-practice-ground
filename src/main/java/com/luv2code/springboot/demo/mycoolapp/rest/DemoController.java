fae81bdb8e029e3a8c64ac53272cd21200c3f83f
Alison Wu <alison.wu23@gmail.com>
2024-11-12 15:23:23 +1000

    feat: 02-component-scanning

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -1,5 +1,6 @@
-package com.luv2code.springboot.demo.mycoolapp;
+package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import com.luv2code.springboot.demo.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
