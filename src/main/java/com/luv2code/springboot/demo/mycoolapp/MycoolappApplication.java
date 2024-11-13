8c55dabe867c37d46d3ddbf6ddb29e2cace6b0b8
Alison Wu <alison.wu23@gmail.com>
2024-11-13 17:05:08 +1000

    feat: setting up spring boot project & dependencies

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MycoolappApplication {
@@ -10,4 +12,12 @@ public class MycoolappApplication {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		// Java Lambda expression begins
+		return runner -> {
+			System.out.println("Hello World");
+		};
+		// Java Lambda expression ends
+	}
 }
