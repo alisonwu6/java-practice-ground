fd062c3f492fbc333a840a696a5dd664fa3302c6
Alison Wu <alison.wu23@gmail.com>
2024-11-16 13:49:58 +1000

    feat(0407): (5) Create REST controller to use DAO

---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/EmployeeRestController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import com.luv2code.springboot.demo.mycoolapp.dao.EmployeeDAO;
+import com.luv2code.springboot.demo.mycoolapp.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    // quick and dirty: inject employee dao (use constructor injection)
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}
