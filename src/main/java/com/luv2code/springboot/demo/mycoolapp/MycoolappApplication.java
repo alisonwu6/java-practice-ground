d65eac8937cd3e11e1acc24df41cb0ed80171ec7
Alison Wu <alison.wu23@gmail.com>
2025-01-24 11:33:50 +1000

    chore(09): set commandLineRunner, connect to sql & clear logging messages

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MycoolappApplication {
@@ -10,4 +12,12 @@ public class MycoolappApplication {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
 
+	// Command Line App
+	// commandLineRunner is executed after the spring beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("hello world");
+		};
+	}
 }
