2e52466733c79e4c582da468d3e525a0dc86c867
Alison Wu <alison.wu23@gmail.com>
2025-01-24 12:44:44 +1000

    feat(09): 01-jpa-one-to-one-uni-create-instructor

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,5 +1,8 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
+import com.luv2code.springboot.demo.mycoolapp.dao.AppDAO;
+import com.luv2code.springboot.demo.mycoolapp.entity.Instructor;
+import com.luv2code.springboot.demo.mycoolapp.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +11,46 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MycoolappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MycoolappApplication.class, args);
+    }
 
-	// Command Line App
-	// commandLineRunner is executed after the spring beans have been loaded
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
-		return runner -> {
-			System.out.println("hello world");
-		};
-	}
+    // Command Line App
+    // commandLineRunner is executed after the spring beans have been loaded
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+        return runner -> {
+            createInstructor(appDAO);
+        };
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+
+        /*
+        // create the instructor
+        Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+        // create the instructor detail
+        InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Luv 2 code!!!");
+         */
+
+        // create the instructor
+        Instructor tempInstructor = new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+
+        // create the instructor detail
+        InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Guitar");
+
+        // associate the objects
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+        // save the instructor
+        //
+        // NOTE: this will ALSO save the details object
+        // because of CascadeType.ALL
+        //
+        System.out.println("Saving instructor: " + tempInstructor);
+        appDAO.save(tempInstructor);
+
+        System.out.println("Done!");
+    }
 }
---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/dao/AppDAO.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.demo.mycoolapp.dao;
+
+import com.luv2code.springboot.demo.mycoolapp.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+    
+}
